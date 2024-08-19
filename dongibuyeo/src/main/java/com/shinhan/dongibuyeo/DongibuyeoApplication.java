abbc34951a3822a8e76afcef6a24cc23203a4247
yeonseo <yeonseo.jung0@gmail.com>
2024-08-19 10:53:40 +0900

    init

---
+++ dongibuyeo/src/main/java/com/shinhan/dongibuyeo/DongibuyeoApplication.java
@@ -0,0 +1,13 @@
+package com.shinhan.dongibuyeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongibuyeoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongibuyeoApplication.class, args);
+    }
+
+}
---
+++ dongibuyeo/src/test/java/com/shinhan/dongibuyeo/DongibuyeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinhan.dongibuyeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongibuyeoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
