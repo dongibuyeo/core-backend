a6db79edd6e72a7a7387ae77033fa7fa87083ca6
yeondori <yeonseo.jung0@gmail.com>
2024-08-21 00:41:35 +0900

    feat: 회원가입 기능

--- dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/member/mapper/MemberMapper.java
+++ dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/member/mapper/MemberMapper.java
@@ -1,5 +1,13 @@
 package com.shinhan.dongibuyeo.domain.member.mapper;
 
-//@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+import com.shinhan.dongibuyeo.domain.member.dto.request.MemberSaveRequest;
+import com.shinhan.dongibuyeo.domain.member.entity.Member;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface MemberMapper {
+
+    Member toMemberEntity(MemberSaveRequest request);
 }
