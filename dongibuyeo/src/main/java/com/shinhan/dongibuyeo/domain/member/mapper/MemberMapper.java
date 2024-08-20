c7530d9fb0eabfeb20b5a46f94610019177e6c29
yeondori <yeonseo.jung0@gmail.com>
2024-08-21 01:11:06 +0900

    feat: 이메일 중복 조회 기능

--- dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/member/mapper/MemberMapper.java
+++ dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/member/mapper/MemberMapper.java
@@ -4,7 +4,6 @@ import com.shinhan.dongibuyeo.domain.member.dto.request.MemberSaveRequest;
 import com.shinhan.dongibuyeo.domain.member.dto.response.MemberLoginResponse;
 import com.shinhan.dongibuyeo.domain.member.entity.Member;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
