ec0c925d04875d92b536309b756ee3ffc8358476
yeondori <yeonseo.jung0@gmail.com>
2024-08-21 00:11:17 +0900

    chore: 패키지 구조 변경 member -> domain/member

--- dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/member/mapper/MemberMapper.java
+++ dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/member/mapper/MemberMapper.java
@@ -1,5 +1,5 @@
 package com.shinhan.dongibuyeo.domain.member.mapper;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+//@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface MemberMapper {
 }
