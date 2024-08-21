8433626b479941edc7645dda945ae65b3ca72b78
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2024-08-21 09:31:36 +0900

    feat: Account Controller

--- dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/account/controller/AccountController.java
+++ dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/account/controller/AccountController.java
@@ -1,10 +1,10 @@
 package com.shinhan.dongibuyeo.domain.account.controller;
 
 import com.shinhan.dongibuyeo.domain.account.service.AccountService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/account")
@@ -16,9 +16,30 @@ public class AccountController {
         this.accountService = accountService;
     }
 
-    @PostMapping
-    public void makeAccount() {}
+    @PostMapping("/personal")
+    public ResponseEntity<Void> makePersonalAccount() {
+        return ResponseEntity.ok().build();
+    }
 
-    @GetMapping
-    public void getAccount() {}
+    @PostMapping("/challenge")
+    public ResponseEntity<Void> makeChallengeAccount() {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/all/{memberId}")
+    public ResponseEntity<Void> getAllAccountsByMemberId(@RequestParam UUID memberId) {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{accountId}")
+    public ResponseEntity<Void> getAccountByAccountId(@RequestParam UUID accountId) {
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> terminateAccountByMemberId() {
+        return ResponseEntity.ok().build();
+    }
+
+
 }
