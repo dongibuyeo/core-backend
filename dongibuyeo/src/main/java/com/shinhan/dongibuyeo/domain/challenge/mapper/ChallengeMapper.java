b6b1820323eea78886b700de981acaf721b873d3
yeondori <yeonseo.jung0@gmail.com>
2024-08-21 23:48:57 +0900

    feat: 전체 챌린지 조회 기능 구현

--- dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/challenge/mapper/ChallengeMapper.java
+++ dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/challenge/mapper/ChallengeMapper.java
@@ -1,8 +1,7 @@
 package com.shinhan.dongibuyeo.domain.challenge.mapper;
 
-import com.shinhan.dongibuyeo.domain.member.dto.request.MemberSaveRequest;
-import com.shinhan.dongibuyeo.domain.member.dto.response.MemberResponse;
-import com.shinhan.dongibuyeo.domain.member.entity.Member;
+import com.shinhan.dongibuyeo.domain.challenge.dto.response.ChallengeResponse;
+import com.shinhan.dongibuyeo.domain.challenge.entity.Challenge;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
@@ -10,4 +9,9 @@ import org.mapstruct.MappingConstants;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ChallengeMapper {
 
+    @Mapping(source = "challenge.id", target = "challengeId")
+    @Mapping(source = "challenge.account.accountNo", target = "accountNo")
+    @Mapping(target = "deposit", expression = "java(challenge.getDeposit().get())")
+    @Mapping(target = "participants", expression = "java(challenge.getParticipants().get())")
+    ChallengeResponse toChallengeResponse(Challenge challenge);
 }
--- dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/challenge/service/ChallengeService.java
+++ dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/challenge/service/ChallengeService.java
@@ -1,14 +1,27 @@
 package com.shinhan.dongibuyeo.domain.challenge.service;
 
+import com.shinhan.dongibuyeo.domain.challenge.dto.response.ChallengeResponse;
+import com.shinhan.dongibuyeo.domain.challenge.mapper.ChallengeMapper;
 import com.shinhan.dongibuyeo.domain.challenge.repository.ChallengeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ChallengeService {
 
     private final ChallengeRepository challengeRepository;
+    private final ChallengeMapper challengeMapper;
 
-    public ChallengeService(ChallengeRepository challengeRepository) {
+    public ChallengeService(ChallengeRepository challengeRepository, ChallengeMapper challengeMapper) {
         this.challengeRepository = challengeRepository;
+        this.challengeMapper = challengeMapper;
+    }
+
+    public List<ChallengeResponse> findAllChallenges() {
+        return challengeRepository.findAll()
+                .stream()
+                .map(challengeMapper::toChallengeResponse)
+                .toList();
     }
 }
