74fd8b909791b2355b0918a11de81bcaa20bc7fe
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2024-08-21 22:31:06 +0900

    feat : Make Product

---
+++ dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/product/controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.shinhan.dongibuyeo.domain.product.controller;
+
+import com.shinhan.dongibuyeo.domain.product.dto.request.MakeProductRequest;
+import com.shinhan.dongibuyeo.domain.product.dto.response.MakeProductResponse;
+import com.shinhan.dongibuyeo.domain.product.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<MakeProductResponse> makeProduct(@RequestBody MakeProductRequest request) {
+        return ResponseEntity.ok(productService.makeProduct(request));
+    }
+}
