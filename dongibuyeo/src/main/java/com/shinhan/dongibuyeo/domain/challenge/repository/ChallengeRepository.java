83b63b7d9b22a15d9e31e1e2c07ac99be45d932e
yeondori <yeonseo.jung0@gmail.com>
2024-08-22 00:09:49 +0900

    feat: 챌린지 개별 조회 기능 구현

--- dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/challenge/repository/ChallengeRepository.java
+++ dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/challenge/repository/ChallengeRepository.java
@@ -3,6 +3,10 @@ package com.shinhan.dongibuyeo.domain.challenge.repository;
 import com.shinhan.dongibuyeo.domain.challenge.entity.Challenge;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
 
+    Optional<Challenge> findChallengeById(UUID challengeId);
 }
--- dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/challenge/service/ChallengeService.java
+++ dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/challenge/service/ChallengeService.java
@@ -1,11 +1,13 @@
 package com.shinhan.dongibuyeo.domain.challenge.service;
 
 import com.shinhan.dongibuyeo.domain.challenge.dto.response.ChallengeResponse;
+import com.shinhan.dongibuyeo.domain.challenge.exception.ChallengeNotFoundException;
 import com.shinhan.dongibuyeo.domain.challenge.mapper.ChallengeMapper;
 import com.shinhan.dongibuyeo.domain.challenge.repository.ChallengeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ChallengeService {
@@ -23,5 +25,11 @@ public class ChallengeService {
                 .stream()
                 .map(challengeMapper::toChallengeResponse)
                 .toList();
+    }
+
+    public ChallengeResponse findChallengeByChallengeId(UUID challengeId) {
+        return challengeRepository.findChallengeById(challengeId)
+                .map(challengeMapper::toChallengeResponse)
+                .orElseThrow(() -> new ChallengeNotFoundException(challengeId));
     }
 }
