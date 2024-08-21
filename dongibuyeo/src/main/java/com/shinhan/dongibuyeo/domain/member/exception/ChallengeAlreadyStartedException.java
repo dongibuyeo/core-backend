e83a6f10dcfec80be73cf71da34d160313807fe6
yeondori <yeonseo.jung0@gmail.com>
2024-08-22 01:17:13 +0900

    feat: 챌린지 참여 취소 기능 구현

---
+++ dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/member/exception/ChallengeAlreadyStartedException.java
@@ -0,0 +1,18 @@
+package com.shinhan.dongibuyeo.domain.member.exception;
+
+import com.shinhan.dongibuyeo.global.exception.BaseException;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class ChallengeAlreadyStartedException extends BaseException {
+
+    public ChallengeAlreadyStartedException(UUID challengeId) {
+        super(
+                "CHALLENGE_ALREADY_STARTED_01",
+                "이미 챌린지가 시작되어 취소할 수 없습니다.",
+                Map.of("challengeId", String.valueOf(challengeId))
+        );
+    }
+
+}
