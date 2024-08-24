9dc5f3a3d2801d8dd6111dce5df734bb6041dc0f
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2024-08-24 15:14:59 +0900

    feat : Quiz 생성, Random Quiz 반환, 사용자의 한달 Quiz 통계 , 사용자가 오늘 퀴즈를 풀었는지 확인, Quiz 풀기

--- dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/quiz/entity/Quiz.java
+++ dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/quiz/entity/Quiz.java
@@ -1,19 +1,22 @@
 package com.shinhan.dongibuyeo.domain.quiz.entity;
 
 import com.github.f4b6a3.ulid.UlidCreator;
+import com.shinhan.dongibuyeo.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
 
 import java.util.UUID;
 
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Quiz {
+@SQLRestriction("deleted_at is null")
+public class Quiz extends BaseEntity {
 
     @Id
     @Column(columnDefinition = "BINARY(16)")
