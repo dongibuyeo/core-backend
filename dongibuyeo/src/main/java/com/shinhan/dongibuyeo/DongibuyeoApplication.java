93d3651148cd60a874f7e98b81db91dc69adb1ab
yeonseo <yeonseo.jung0@gmail.com>
2024-08-25 11:32:08 +0900

    refactor: 스케줄러 통합

--- dongibuyeo/src/main/java/com/shinhan/dongibuyeo/DongibuyeoApplication.java
+++ dongibuyeo/src/main/java/com/shinhan/dongibuyeo/DongibuyeoApplication.java
@@ -2,8 +2,10 @@ package com.shinhan.dongibuyeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DongibuyeoApplication {
 
     public static void main(String[] args) {
