19bf0d16fdc07c36474f24c76c3aaa9b78279e00
yeonseo <yeonseo.jung0@gmail.com>
2024-08-27 08:56:45 +0900

    refactor: 정산 로직 수정

--- dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/challenge/entity/MemberChallengeStatus.java
+++ dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/challenge/entity/MemberChallengeStatus.java
@@ -3,5 +3,5 @@ package com.shinhan.dongibuyeo.domain.challenge.entity;
 public enum MemberChallengeStatus {
     BEFORE_CALCULATION,       // 환급액 계산 전
     CALCULATED,               // 환급액 계산 완료(환급 전)
-    REFUNDED                  // 환급 완료
+    REWARDED                  // 환급 완료
 }
