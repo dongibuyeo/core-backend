3264cf283c666bf44b19780c5e44e28cc087c3f9
yeonseo <yeonseo.jung0@gmail.com>
2024-08-26 17:58:36 +0900

    feat: Status 필드 추가

---
+++ dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/challenge/entity/MemberChallengeStatus.java
@@ -0,0 +1,7 @@
+package com.shinhan.dongibuyeo.domain.challenge.entity;
+
+public enum MemberChallengeStatus {
+    BEFORE_CALCULATION,       // 환급액 계산 전
+    CALCULATED,               // 환급액 계산 완료(환급 전)
+    REFUNDED                  // 환급 완료
+}
