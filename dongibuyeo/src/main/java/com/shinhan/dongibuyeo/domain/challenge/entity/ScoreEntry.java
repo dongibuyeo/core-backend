ba64af558eb2192bb91280372499ca1ff64f2bbc
yeonseo <yeonseo.jung0@gmail.com>
2024-08-26 17:58:18 +0900

    feat: ScoreEntry 추가

---
+++ dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/challenge/entity/ScoreEntry.java
@@ -0,0 +1,14 @@
+package com.shinhan.dongibuyeo.domain.challenge.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScoreEntry {
+    private String item;
+    private int score;
+    private int currentTotalScore;
+}
