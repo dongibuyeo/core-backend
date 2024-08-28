75ccd7ebc8c4a0b4918a9688f7df4374e153745d
yeonseo <yeonseo.jung0@gmail.com>
2024-08-28 15:00:45 +0900

    feat: TimeZone 설정

--- dongibuyeo/src/main/java/com/shinhan/dongibuyeo/DongibuyeoApplication.java
+++ dongibuyeo/src/main/java/com/shinhan/dongibuyeo/DongibuyeoApplication.java
@@ -1,14 +1,22 @@
 package com.shinhan.dongibuyeo;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 @EnableAspectJAutoProxy
 public class DongibuyeoApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DongibuyeoApplication.class, args);
