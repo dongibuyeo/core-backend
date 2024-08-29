0ff0858567f3a218adec52b5d0e488256fcdb5ee
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2024-08-29 12:09:17 +0900

    fix : Chat FIX

--- dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/chat/entity/Room.java
+++ dongibuyeo/src/main/java/com/shinhan/dongibuyeo/domain/chat/entity/Room.java
@@ -1,5 +1,6 @@
 package com.shinhan.dongibuyeo.domain.chat.entity;
 
+import com.github.f4b6a3.ulid.UlidCreator;
 import com.shinhan.dongibuyeo.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -19,12 +20,13 @@ public class Room extends BaseEntity {
 
     @Id
     @Column(columnDefinition = "BINARY(16)")
-    private UUID id;
+    private UUID id = UlidCreator.getMonotonicUlid().toUuid();
     private String name;
 
     @OneToMany(
             mappedBy = "room",
-            cascade = {CascadeType.PERSIST, CascadeType.MERGE}
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+            fetch = FetchType.LAZY
     )
     private List<Message> messages = new ArrayList<>();
 
