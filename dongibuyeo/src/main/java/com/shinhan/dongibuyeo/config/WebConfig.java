fee834972ca7273562e104f92836dd1fc5598436
yeonseo <yeonseo.jung0@gmail.com>
2024-08-31 15:06:28 +0900

    fix: URL 배열처리

--- dongibuyeo/src/main/java/com/shinhan/dongibuyeo/config/WebConfig.java
+++ dongibuyeo/src/main/java/com/shinhan/dongibuyeo/config/WebConfig.java
@@ -7,11 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    String[] allowedUrls = {"http://localhost:3000", "http://13.125.129.235:3000", "http://3.36.59.109:3000"};
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("http://13.125.129.235:3000")
+                .allowedOrigins(allowedUrls)
+                .allowedOrigins()
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
